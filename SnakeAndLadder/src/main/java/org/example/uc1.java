b51b1b1730448bda74c243c069bd937dd76d112f
“FaizanMomin1” <faizanmomin8991@gmail.com>
2023-04-15 11:28:02 +0530

    winning position at 0

---
+++ SnakeAndLadder/src/main/java/org/example/uc1.java
@@ -0,0 +1,15 @@
+package org.example;
+
+public class uc1 {
+    public static void main(String[] args) {
+        System.out.println("****WELCOME TO SNAKE AND LADDER PROBLEM *****");
+        int player1 = 0;
+        int position = 0;
+        System.out.println();
+        System.out.println("player start its position from " +position + "player1");
+
+
+
+    }
+
+}
